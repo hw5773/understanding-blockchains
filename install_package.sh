2b401d904f5e62863ee16a8d4907c8529f7c10bd
Hyunwoo <hwlee2014@mmlab.snu.ac.kr>
2020-06-08 21:37:06 +0900

    add the install package script

---
+++ install_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+wget https://services.gradle.org/distributions/gradle-6.4.1-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-*.zip
+echo "export GRADLE_HOME=/opt/gradle/gradle-6.4.1" > env
+echo "export PATH=${GRADLE_HOME}/bin:$PATH" >> env
+source ./env
+
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt install openjdk-11-jdk

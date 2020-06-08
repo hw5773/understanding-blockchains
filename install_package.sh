df90bcd4308b297a3c796750584b4c9f57b1ff1b
Hyunwoo <hwlee2014@mmlab.snu.ac.kr>
2020-06-08 21:53:36 +0900

    revise the install_package.sh file

--- install_package.sh
+++ install_package.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 wget https://services.gradle.org/distributions/gradle-6.4.1-bin.zip -P /tmp
 sudo unzip -d /opt/gradle /tmp/gradle-*.zip
 echo "export GRADLE_HOME=/opt/gradle/gradle-6.4.1" > env
-echo "export PATH=${GRADLE_HOME}/bin:$PATH" >> env
+echo "export PATH=/opt/gradle/gradle-6.4.1/bin:$PATH" >> env
 source ./env
 
 sudo add-apt-repository ppa:openjdk-r/ppa
